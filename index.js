ae78258c8359985633eed17cc80803d610b0301e
Viraj Jain <virajjain7.vj@gmail.com>
2022-01-06 12:44:20 +0530

    filled item list

--- index.js
+++ index.js
@@ -2,12 +2,33 @@ const navItems = document.querySelectorAll(".nav-item");
 
 const itemList = [
   {
-    name: "",
-    category: "",
-    price: "",
-    img: "",
-    description: "",
+    name: "burger",
+    category: "Breakfast",
+    price: "199$",
+    img: "img/burger.jpg",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis rem totam ducimus nam illum mollitia!",
   },
+  {
+    name: "italian",
+    category: "Lunch",
+    price: "199$",
+    img: "img/italian.jpg",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis rem totam ducimus nam illum mollitia!",
+  },
+  {
+    name: "pasta",
+    category: "Shakes",
+    price: "199$",
+    img: "img/pasta.jpg",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis rem totam ducimus nam illum mollitia!",
+  },
+  {
+    name: "sandwich",
+    category: "Dinner",
+    price: "199$",
+    img: "img/sanwich.jpg",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis rem totam ducimus nam illum mollitia!",
+  }
 ];
 navItems.forEach((item) => {
   item.addEventListener("click", clickHandler);
@@ -15,4 +36,10 @@ navItems.forEach((item) => {
 
 function clickHandler(event) {
   const menuCategory = event.target.innerText;
+  itemList.forEach(item=>{
+  if (item.category===menuCategory){
+    console.log(item);
+
+  }
+})
 }
